3c04059167cbc70032a4eb160a9361f1a58cfcab
Vimlesh Kumar <vimlesh@vimsco-consulting.com>
2026-02-13 23:47:53 -0500

    iac: finalize prod environment config and outputs

---
+++ envs/prod/variables.tf
@@ -0,0 +1,16 @@
+
+variable "environment" {
+  type        = string
+  description = "Environment name (dev/prod)"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "VPC CIDR block for the environment"
+}
+
+
+variable "region" {
+  type        = string
+  description = "us-east-1 or such"
+}
